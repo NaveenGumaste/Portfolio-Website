a10c01e8302bb628ecddf2d70b37190c08f084b4
Spacer <64587607+NaveenGumaste@users.noreply.github.com>
2023-09-23 19:49:54 +0000

    initial Updates

---
+++ app.js
@@ -0,0 +1,15 @@
+// JavaScript for smooth scrolling to section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      window.scrollTo({
+        top: targetElement.offsetTop - 50,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
