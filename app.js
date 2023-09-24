c12f2c32a43ec119336ad6ae9f00cb2f1bc5cde4
Spacer <64587607+NaveenGumaste@users.noreply.github.com>
2023-09-24 08:34:20 +0000

    Updated tge codes

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-const sections = document.querySelectorAll(".section");
+document.addEventListener("DOMContentLoaded", () => {
+	const cards = document.querySelectorAll(".card");
 
-// Helper function to check if an element is in the viewport
-function isElementInViewport(el) {
-	const rect = el.getBoundingClientRect();
-	return (
-		rect.top >= 0 &&
-		rect.left >= 0 &&
-		rect.bottom <=
-			(window.innerHeight || document.documentElement.clientHeight) &&
-		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-	);
-}
+	cards.forEach((card) => {
+		card.addEventListener("mouseenter", () => {
+			card.querySelector(".face.face2").style.height = "60px";
+		});
 
-// Function to add animations when a section is in the viewport
-function animateSections() {
-	sections.forEach((section) => {
-		if (isElementInViewport(section)) {
-			section.style.opacity = "1";
-			section.style.transform = "translateY(0)";
-		}
+		card.addEventListener("mouseleave", () => {
+			card.querySelector(".face.face2").style.height = "100%";
+		});
 	});
-}
-
-// Initial call to set visible sections on page load
-animateSections();
-
-// Event listener to trigger animations on scroll
-window.addEventListener("scroll", animateSections);
+});
