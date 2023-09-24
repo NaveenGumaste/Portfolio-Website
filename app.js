f95c4c5410450b786e807c15060373471135b6e5
Spacer <64587607+NaveenGumaste@users.noreply.github.com>
2023-09-24 05:44:39 +0000

    Changes to the code

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-// JavaScript for smooth scrolling to section
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-  
-      window.scrollTo({
-        top: targetElement.offsetTop - 50,
-        behavior: 'smooth'
-      });
-    });
-  });
-  
+const sections = document.querySelectorAll(".section");
+
+// Helper function to check if an element is in the viewport
+function isElementInViewport(el) {
+	const rect = el.getBoundingClientRect();
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <=
+			(window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
+// Function to add animations when a section is in the viewport
+function animateSections() {
+	sections.forEach((section) => {
+		if (isElementInViewport(section)) {
+			section.style.opacity = "1";
+			section.style.transform = "translateY(0)";
+		}
+	});
+}
+
+// Initial call to set visible sections on page load
+animateSections();
+
+// Event listener to trigger animations on scroll
+window.addEventListener("scroll", animateSections);
