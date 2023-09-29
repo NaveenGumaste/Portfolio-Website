3d15561945ff88e17fa9305257e6dc26c3002e4b
Naveen <64587607+BrownUchiha@users.noreply.github.com>
2023-09-29 13:37:38 +0530

    update ,js

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-// 	animateText(); // Call animateText function when DOM is fully loaded
-
-// 	const cards = document.querySelectorAll(".card");
-
-// 	cards.forEach((card) => {
-// 		card.addEventListener("mouseenter", () => {
-// 			card.querySelector(".face.face2").style.height = "60px";
-// 		});
-
-// 		card.addEventListener("mouseleave", () => {
-// 			card.querySelector(".face.face2").style.height = "100%";
-// 		});
-// 	});
-// });
-
-// function animateText() {
-// 	const paragraph = document.querySelector(".animated-text");
-// 	const text = paragraph.innerText;
-// 	paragraph.innerHTML = "";
-
-// 	for (let i = 0; i < text.length; i++) {
-// 		const charSpan = document.createElement("span");
-// 		charSpan.textContent = text[i] === " " ? "&nbsp;" : text[i]; // Preserve spaces
-// 		charSpan.style.animationDelay = `${i * 0.05}s`;
-// 		paragraph.appendChild(charSpan);
-
-// 		// Log each character to the console
-// 		console.log(charSpan.textContent);
-// 	}
-// }
-
 document.addEventListener("DOMContentLoaded", () => {
 	animateText(); // Call animateText function when DOM is fully loaded
 
@@ -64,3 +32,44 @@ function animateText() {
 		paragraph.appendChild(charSpan);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+// 	animateText(); // Call animateText function when DOM is fully loaded
+
+// 	const cards = document.querySelectorAll(".card");
+
+// 	cards.forEach((card) => {
+// 		card.addEventListener("mouseenter", () => {
+// 			card.querySelector(".face.face2").style.height = "60px";
+// 		});
+
+// 		card.addEventListener("mouseleave", () => {
+// 			card.querySelector(".face.face2").style.height = "100%";
+// 		});
+// 	});
+// });
+
+// function animateText() {
+// 	const paragraph = document.querySelector(".animated-text");
+// 	const text = paragraph.innerText;
+// 	paragraph.innerHTML = "";
+
+// 	for (let i = 0; i < text.length; i++) {
+// 		const charSpan = document.createElement("span");
+// 		charSpan.textContent = text[i] === " " ? "&nbsp;" : text[i]; // Preserve spaces
+// 		charSpan.style.animationDelay = `${i * 0.05}s`;
+// 		paragraph.appendChild(charSpan);
+
+// 		// Log each character to the console
+// 		console.log(charSpan.textContent);
+// 	}
+// }
